ecff2b592911f1602173b469c75c7d188e1d038e
stiles001 <437914803@qq.com>
2021-10-11 21:06:35 +0800

    add ant design in project

--- src/unauthenticated-app/index.tsx
+++ src/unauthenticated-app/index.tsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
 import { LoginScreen } from "./login";
 import { RegisterScreen } from "./register";
+import { Card, Button } from "antd";
 
 export const UnauthenticatedApp = () => {
   const [isRegister, setIsRegister] = useState(false);
 
   return (
-    <div>
-      {isRegister ? <RegisterScreen /> : <LoginScreen />}
-      <button onClick={() => setIsRegister(!isRegister)}>
-        {isRegister ? "已有账号？直接去登录" : "没有账号？注册新账号"}
-      </button>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <Card>
+        {isRegister ? <RegisterScreen /> : <LoginScreen />}
+        <Button onClick={() => setIsRegister(!isRegister)}>
+          {isRegister ? "已有账号？直接去登录" : "没有账号？注册新账号"}
+        </Button>
+      </Card>
     </div>
   );
 };
--- src/unauthenticated-app/register.tsx
+++ src/unauthenticated-app/register.tsx
@@ -1,27 +1,32 @@
 import { useAuth } from "context/auth-context";
-import { FormEvent } from "react";
+import { Form, Input, Button } from "antd";
 
 export const RegisterScreen = () => {
   const { register } = useAuth();
 
-  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
-    evt.preventDefault();
-    const username = (evt.currentTarget.elements[0] as HTMLInputElement).value;
-    const password = (evt.currentTarget.elements[1] as HTMLInputElement).value;
-    register({ username, password });
+  const handleSubmit = (value: { username: string; password: string }) => {
+    register(value);
   };
 
   return (
-    <form action="" onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="username">用户名</label>
-        <input type="text" id="username" />
-      </div>
-      <div>
-        <label htmlFor="password">密码</label>
-        <input type="password" name="" id="password" />
-      </div>
-      <button type="submit">注册</button>
-    </form>
+    <Form action="" onFinish={handleSubmit}>
+      <Form.Item
+        name="username"
+        rules={[{ required: true, message: "please input name" }]}
+      >
+        <Input placeholder="用户名" type="text" id="username" />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[{ required: true, message: "please input password" }]}
+      >
+        <Input placeholder="密码" type="password" id="password" />
+      </Form.Item>
+      <Form.Item>
+        <Button htmlType="submit" type="primary">
+          注册
+        </Button>
+      </Form.Item>
+    </Form>
   );
 };
